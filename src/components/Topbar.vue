321d142e3c30e057589b34c3b6323ff3b30ba52b
Krystof Zahumensky <zahumensky.krystof@gmail.com>
2021-02-11 17:49:31 +0100

    rawr

---
+++ src/components/Topbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="topbar">
+    <a class="topbar-logo"></a>
+    <div class="topbar-menu"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topbar",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/main.scss";
+.topbar{
+  top: 72px;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  z-index: $zi-topbar;
+  &-logo{
+    height: 32px;
+    width: 32px;
+  }
+  &-menu{
+    ::before, ::after{
+      width: 32px;
+      height: 2px;
+      background-color: $c-primary;
+    }
+  }
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    This is home prdel
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
