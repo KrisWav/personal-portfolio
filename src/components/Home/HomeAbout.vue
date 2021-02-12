05936f8e531abfb047f1b95ecd7c424073d794bb
Krystof Zahumensky <zahumensky.krystof@gmail.com>
2021-02-12 15:01:44 +0100

    Device width about and cover responsivity

--- src/components/Home/HomeAbout.vue
+++ src/components/Home/HomeAbout.vue
@@ -5,7 +5,7 @@
         Hi, I’m Krystof Zahumensky.
         Programmer, front-end developer, student and entrepreneur and degenerate person from Hradec Kralove, Czech Republic.
       </p>
-      <p class="t-about">I co-own company <a class="link-inline" href="https://find-and-update.company-information.service.gov.uk/company/11392040"> Breezy Investments Limited</a>, based in the UK, under which I am currently working on multiple risk oriented projects.</p>
+      <p class="t-about">I co-own company <a class="link-inline" target="_blank" href="https://find-and-update.company-information.service.gov.uk/company/11392040"> Breezy Investments Limited</a>, based in the UK, under which I am currently working on multiple risk oriented projects.</p>
       <p class="t-about">Apart from that I love my friends and good music. Cavetown is my most favourite artist. I’m an animal lover, piano and guitar player. Sometimes I create my own music.</p>
       <div class="btn-container">
         <button class="btn btn-primary">More about me</button>
