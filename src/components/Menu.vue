3a4cb53861fdc769a9675ebe40b33149dbbc15e5
Krystof Zahumensky <zahumensky.krystof@gmail.com>
2021-02-23 13:48:21 +0100

    menu animation using gsap

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,8 +1,12 @@
 <template>
-  <transition name="a-menu">
+  <transition appear name="a-menu">
     <div v-if="getMenuState" class="menu grid">
       <div class="menu-logo"></div>
-      <transition-group appear @enter="linkEnter()" name="a-menu-links" tag="ul" class="menu-links">
+      <transition-group
+        appear
+        @enter="linksEnter"
+        @before-enter="linksBeforeEnter"
+        tag="ul" class="menu-links" >
         <li v-for="(link, index) in menuLinks" :key="link.name" :data-index="index">
           <router-link v-if="link.routerLink" :to="link.to" @click.native="setMenuState()">{{link.name}}</router-link>
           <a v-else :href="link.to" target="_blank" @click="setMenuState()">{{link.name}}</a>
@@ -14,6 +18,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import gsap from 'gsap'
 
 export default {
   name: 'Menu',
@@ -22,8 +27,19 @@ export default {
   },
   methods: {
     ...mapActions(['setMenuState']),
-    linkEnter: function () {
-      setTimeout(null, 500)
+    linksEnter: (el, done) => {
+      gsap.to(el, {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        animationTimingFunction: 'cubic-bezier(.07,.63,.36,.96)',
+        onComplete: done,
+        delay: el.dataset.index * 0.15
+      })
+    },
+    linksBeforeEnter: (el) => {
+      el.style.opacity = 0
+      el.style.transform = 'translateX(48px)'
     }
   },
   data: () => {
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
-import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from 'gsap/all'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin)
 
 new Vue({
-  gsap,
   router,
   store: store,
   render: h => h(App)
